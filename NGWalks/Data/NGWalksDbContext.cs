9d1414d5095245cfc6a1fd557f31b42de5efe41d
ASUS-GAMING <ASUS-GAMING@HILLS-MADU>
2025-04-06 12:24:54 +0100

    Add project files.

---
+++ NGWalks/Data/NGWalksDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NGWalks.Models.Domain;
+
+namespace NGWalks.Data
+{
+    public class NGWalksDbContext : DbContext
+    {
+        public NGWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Domain.Walk>().HasKey(w => w.id);
+            modelBuilder.Entity<Models.Domain.Region>().HasKey(r => r.Id);
+            modelBuilder.Entity<Models.Domain.Difficulty>().HasKey(d => d.Id);
+        }
+    }
+    
+
+    
+}
---
+++ NGWalks/Models/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NGWalks.Models.Domain
+{
+    public class Walk
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+        public Guid DiffcultyId { get; set; }
+        public Guid RegionId { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+    }
+}
